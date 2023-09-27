f5a82d4cea7a8472d746d0fb7e54c3f750b0c3b1
Md Nazrul Islam <arfanislamabir@gmail.com>
2023-09-28 00:19:38 +0600

    implementing MLFLOW

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## End to End Data Science Project 
+
+MLFLOW_TRACKING_URI=https://dagshub.com/arfanislamabir/end_to_end_ml_project.mlflow \
+MLFLOW_TRACKING_USERNAME=arfanislamabir \
+MLFLOW_TRACKING_PASSWORD=f52de5d3fb13c882be8c25c63fff2bab9735d3c5 \
+python script.py
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 
 
@@ -16,7 +17,13 @@ if __name__ == "__main__":
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
         # data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+        #Modl Training
+
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
 
     except Exception as e:
         logging.info("Custom Exeception")
